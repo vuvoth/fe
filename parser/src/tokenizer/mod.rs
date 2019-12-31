5e14de4a2cbb5d2a5d2fc134b165584f1fd9a66c
David Sanders <davesque@gmail.com>
2019-12-30 17:23:04 -0700

    Update tokenizer/mod.rs doc comment

--- parser/src/tokenizer/mod.rs
+++ parser/src/tokenizer/mod.rs
@@ -1,5 +1,4 @@
-//! A Rust port of [Python's std lib `tokenize`
-//! module](https://github.com/python/cpython/blob/2a58b0636d1f620f8a85a2e4c030cc10551936a5/Lib/tokenize.py).
+//! Heavily inspired by (some might say ported from) [Python's std lib `tokenize` module](https://github.com/python/cpython/blob/2a58b0636d1f620f8a85a2e4c030cc10551936a5/Lib/tokenize.py).
 //!
 //! ## Usage
 //!
@@ -19,12 +18,19 @@
 //!   we call token kinds) produced by python's `tokenize.tokenize` method
 //!   *except* for an initial `ENCODING` token.  Source files are always assumed
 //!   to be encoded in utf-8 so this token is redundant.
+//! * Unlike the [Python std lib `tokenize`
+//!   module](https://github.com/python/cpython/blob/
+//!   2a58b0636d1f620f8a85a2e4c030cc10551936a5/Lib/tokenize.py), our tokenizer
+//!   uses separate tokens for different operator tokens, instead of using a
+//!   single op token to represent all operator tokens.
 //! * The [`self::types::Token`] struct, which we use to represent parsed
-//!   tokens, includes a single `span` field containing the byte offsets
-//!   representing the beginning and exclusive end of the token in a source
-//!   string. This differs from Python's `TokenInfo` instances which use
-//!   line/column tuples to represent the beginning and ending positions of a
-//!   token.
+//!   tokens, in most cases only includes span information to indicate the
+//!   string content of a token.  Token consumers are therefore generally
+//!   expected to have access to the source string from which tokens were
+//!   generated if they wish to obtain the string content of tokens. The only
+//!   token type that independently includes a means to obtain string content
+//!   (name tokens) includes indices into a string interner table (called
+//!   symbols) which can also be used to obtain string content.
 //! * The [`self::tokenize::tokenize`] function has no streaming behavior and
 //!   accepts a reference to an entire source string available in memory.
 //!   Python's `tokenize.tokenize` function accepts a reference to a function
@@ -32,8 +38,7 @@
 //!
 //! As Python's `tokenize` module's implementation is pretty ugly, so is the
 //! implementation of [`self::tokenize::tokenize`].  It may be a candidate for
-//! later cleanup.  However, it may also be fine as is assuming it doesn't need
-//! to be modified often.
+//! later cleanup.
 
 mod regex;
 pub mod tokenize;
